58c302c9a1d9edb91ef0f0a4928bebeb8ae3c6e2
CCDM24 <ccdm24@CCDM24.local>
2019-10-01 13:50:32 -0600

    Pantalla 1

---
+++ Ejercicio1/Ejercicio1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ejercicio1
+//
+//  Created by CCDM24 on 10/1/19.
+//  Copyright © 2019 CCDM24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
